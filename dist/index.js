1396b1a084821d2c77c698ed3aef2d741881e981
unknown <claygifford@gmail.com>
2021-04-13 21:20:25 -0400

    compiled the dist folder

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const App_1 = require("./App");
+const app_1 = require("./app");
 const port = process.env.PORT || 3000;
-App_1.default.listen(port, (err) => {
+app_1.default.listen(port, (err) => {
     if (err) {
         return console.log(err);
     }
